6beb072e1253324eb8de2a70474e111634934feb
Sedar Yildirim <155425285+sedaryildirim@users.noreply.github.com>
2024-08-01 15:40:28 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const shareButton = document.querySelector('.share-button');
-    const shareSection = document.querySelector('.share');
-    const mobileActiveSection = document.querySelector('.mobile-active');
+    const shareButtons = document.querySelectorAll('.share-button');
+    const shareInactive = document.querySelector('.share-inactive');
+    const shareActive = document.querySelector('.share-active');
 
-    function toggleShareView() {
-        shareSection.classList.toggle('hidden');
-        mobileActiveSection.classList.toggle('hidden');
+    function toggleShareOptions() {
+        shareInactive.classList.toggle('hidden');
+        shareActive.classList.toggle('hidden');
     }
 
-    // Add click event listener to the main share button
-    shareButton.addEventListener('click', toggleShareView);
-
-    // Add click event listener to the share button in the mobile active view
-    const mobileShareButton = mobileActiveSection.querySelector('.share-button');
-    if (mobileShareButton) {
-        mobileShareButton.addEventListener('click', toggleShareView);
-    }
+    shareButtons.forEach(button => {
+        button.addEventListener('click', toggleShareOptions);
+    });
 });
